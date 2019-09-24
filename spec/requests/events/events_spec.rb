120f42c13cd92a701dd425650f95a0ee342c4ad2
aonomike <aonomike@gmail.com>
2019-09-24 18:35:42 +0300

    Refactor get /event test to use subject

--- spec/requests/events/events_spec.rb
+++ spec/requests/events/events_spec.rb
@@ -4,27 +4,28 @@ require 'rails_helper'
 
 RSpec.describe EventsController do
   let(:endpoint) { '/events' }
+  let(:event_params) {{name: 'Random Event', time_zone: 'UTC + 3', repeats: 'once', category: 'Pair programing', start_datetime: Date.parse('31-12-2010'), duration: 60}}
 
   context 'GET /events' do
     context 'succesful' do
-      before do
-        get endpoint
-      end
+      subject { get endpoint }
       it 'responds with success' do
+        subject
         expect(response).to have_http_status(:success)
       end
 
       it 'JSON body is empty array if there are no records in database' do
+        subject
         json_response = JSON.parse(response.body)
         expect(json_response).to eq([])
       end
 
       it 'JSON response has correct attributes' do
-        Event.create(name: 'Random Event', time_zone: 'UTC + 3', repeats: 'once', category: 'Pair programing', start_datetime: Date.parse('31-12-2010'), duration: 60)
-        get endpoint
+        Event.create(event_params)
+        subject
         json_response = JSON.parse(response.body)
         json_response.each do |event|
-          expect(event.keys).to match_array(%w[category created_at creator_attendance creator_id description duration exclusions for id modifier_id name project_id repeat_ends repeat_ends_on repeats repeats_every_n_weeks repeats_weekly_each_days_of_the_week_mask slug start_datetime time_zone updated_at url])
+        expect(event.keys).to match_array(%w[category created_at creator_attendance creator_id description duration exclusions for id modifier_id name project_id repeat_ends repeat_ends_on repeats repeats_every_n_weeks repeats_weekly_each_days_of_the_week_mask slug start_datetime time_zone updated_at url])
         end
       end
     end
