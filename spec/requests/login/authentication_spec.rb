60f85b2dac9c744722438b5d1db722518de97875
aonomike <aonomike@gmail.com>
2019-11-19 12:41:33 +0300

    Start implementing tests for /login
    - Test that it reurns 500
    - Test still fails

---
+++ spec/requests/login/authentication_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe 'POST /login', type: :request do
+  let(:user) { create(:user) }
+  let(:url) { '/login' }
+  let(:params) do
+    {
+      user: {
+        email: user.emai,
+        password: user.password
+      }
+    }
+  end
+
+  context 'when params are correct' do
+    before do
+      post url, params: params
+    end
+
+    it 'returns 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+end
