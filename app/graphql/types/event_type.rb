7461f36d76d8eba7f591d8f6a6cb76b038661bf5
mattwr18 <mattwr18@gmail.com>
2020-03-10 14:06:52 +0100

    Set up event type/seeds
    - with only required fields for now

---
+++ app/graphql/types/event_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class EventType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :category, String, null:false
+    field :time_zone, String, null: false
+    field :repeats, String, null: false
+    field :start_datetime, GraphQL::Types::ISO8601DateTime, null: false
+    field :duration, Int, null: false
+  end
+end
