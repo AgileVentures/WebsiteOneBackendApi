ed974e12335f55dcde56ba1185217cfb73bd2d02
Riccardo Giomi <contact@riccardogiomi.com>
2020-05-13 19:20:09 +0100

    Setup Rails encrypted secrets.
    Co-authored-by: Evans Otieno <okothkongo@gmail.com>
    Co-authored-by: Mike Aono <aonomike@gmail.com>
    Co-authored-by: James Dindi <dindijjames@gmail.com>

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -298,14 +298,14 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-      jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
-      jwt.dispatch_requests = [
-        ['POST', %r{^/login$}],
-        ['POST', %r{^/signup$}]
-      ]
-      jwt.revocation_requests = [
-        ['DELETE', %r{^/logout$}]
-      ]
-      jwt.expiration_time = 1.day.to_i
-    end
+    jwt.secret = Rails.application.credentials.devise_jwt[:key]
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}],
+      ['POST', %r{^/signup$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 1.day.to_i
+  end
 end
