fe0f7458f8b57b19437f4bfd4701b3b52f529f3e
Riccardo Giomi <contact@riccardogiomi.com>
2020-05-20 19:26:22 +0100

    Better formatting for Project model validations.
    Co-authored-by: Mike Aono <aonomike@gmail.com>
    Co-authored-by: Gideon Kimutai <gideon.kimutai.kim@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Project < ApplicationRecord
-validates_presence_of :title, :description, :status
+  validates_presence_of :status,      presence: true
+  validates_presence_of :description, presence: true
+  validates_presence_of :title,       presence: true
 end
