74f4d3d24b4634e11dc2d4f5c74093c8583b1323
Riccardo Giomi <contact@riccardogiomi.com>
2020-05-20 19:32:33 +0100

    Formatted Project validations, again.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
-  validates_presence_of :status,      presence: true
-  validates_presence_of :description, presence: true
-  validates_presence_of :title,       presence: true
+  validates :status,      presence: true
+  validates :description, presence: true
+  validates :title,       presence: true
 end
