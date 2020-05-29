0682b9847265a88376f7d1c1bd36cab33573b77a
gr1d99 <gideon.kimutai.kim@gmail.com>
2020-05-29 16:13:43 +0300

    remove unused devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users,
+						 skip: %i[sessions registrations],
              path: '',
              path_names: {
                sign_in: 'login',
@@ -12,6 +13,13 @@ Rails.application.routes.draw do
                sessions: 'sessions',
                registrations: 'registrations'
              }
+
+	as :user do
+		post 'login', to: 'sessions#create', as: :user_session
+		delete 'logout', to: 'sessions#destroy', as: :destroy_user_session
+		post 'signup', to: 'registrations#create', as: :user_registration
+	end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :events, only: %w[index create update show destroy]
