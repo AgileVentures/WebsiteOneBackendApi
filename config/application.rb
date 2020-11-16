4e3d9635313140a80b900f27f6d7823ddef93f37
Matt Lindsey <matthew.r.lindsey@gmail.com>
2020-11-15 20:09:58 -0500

    fix graphql events

--- config/application.rb
+++ config/application.rb
@@ -34,17 +34,17 @@ module WebsiteOneBackendApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*',
-                 headers: %w[Authorization],
-                 methods: :any,
-                 expose: %w[Authorization],
-                 max_age: 600
-      end
-    end
+    #config.api_only = true
+    #config.middleware.insert_before 0, Rack::Cors do
+    #  allow do
+    #    origins '*'
+    #    resource '*',
+    #             headers: %w[Authorization],
+    #             methods: :any,
+    #             expose: %w[Authorization],
+    #             max_age: 600
+    #  end
+    #end
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
