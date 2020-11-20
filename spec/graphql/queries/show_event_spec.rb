63a8ee77bc230cc092ae60211ee1e0dbd2a38c9c
Matt Lindsey <matthew.r.lindsey@gmail.com>
2020-11-19 20:42:16 -0500

    fix rubocop warnings

--- spec/graphql/queries/show_event_spec.rb
+++ spec/graphql/queries/show_event_spec.rb
@@ -4,11 +4,11 @@ require 'rails_helper'
 require 'graphql_helper'
 
 RSpec.describe 'Events' do
-   
-  it "queries and returns all events" do
-    e = create(:event, name: 'FirstEvent')
-    e2 = create(:event, name: 'AnotherEvent')
-    
+
+  it 'queries and returns all events' do
+    create(:event, name: 'FirstEvent')
+    create(:event, name: 'AnotherEvent')
+
     query_string = <<-GRAPHQL
     query
      {
@@ -19,9 +19,9 @@ RSpec.describe 'Events' do
         }
       }
       GRAPHQL
-      
-    result = WebsiteOneBackendApiSchema.execute(query_string)["data"]["events"]
-   
+
+    result = WebsiteOneBackendApiSchema.execute(query_string)['data']['events']
+
     expect(result).to json_contains('FirstEvent', 'name')
     expect(result).to json_contains('AnotherEvent', 'name')
   end
--- spec/graphql_helper.rb
+++ spec/graphql_helper.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define :json_contains do |expected, key|
-    match do |actual|
-      a = actual.collect {|x| x[key]}
-      a.detect { |e| e == expected }
-    end
+  match do |actual|
+    a = actual.collect {|x| x[key]}
+    a.detect { |e| e == expected }
+  end
 end
