50dbb26b4a953249b5745ab2eedca0c87c6670b6
Matt Lindsey <matthew.r.lindsey@gmail.com>
2020-11-21 15:16:17 -0500

    add order_by to events query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
-
 module Types
   class QueryType < Types::BaseObject
-    field :events, [EventType], null: false,
-                                description: 'Query for all events'
-    def events
-      Event.all
+    field :events, [EventType], null: false, description: 'Query for all events' do
+      argument :order_by, String, required: false
+      argument :order_direction, String, required: false
+    end
+    def events(order_by: nil, order_direction: 'asc')
+      if order_by
+        Event.all.order(order_by + ' ' + order_direction)
+      else
+        Event.all
+      end
     end
   end
 end
