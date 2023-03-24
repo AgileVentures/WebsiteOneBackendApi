c24d21407f060d859614161ec8dcb4046302051a
Matt Lindsey <matthew.r.lindsey@gmail.com>
2023-03-24 14:12:05 -0400

    update rubocop and autofix

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -13,7 +13,8 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = WebsiteOneBackendApiSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = WebsiteOneBackendApiSchema.execute(query, variables:, context:,
+operation_name:)
     render json: result
   rescue StandardError => e
     raise e unless Rails.env.development?
@@ -46,6 +47,6 @@ class GraphqlController < ApplicationController
     logger.error e.message
     logger.error e.backtrace.join("\n")
 
-    render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: 500
+    render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: :internal_server_error
   end
 end
--- app/graphql/website_one_backend_api_schema.rb
+++ app/graphql/website_one_backend_api_schema.rb
@@ -15,7 +15,7 @@ class WebsiteOneBackendApiSchema < GraphQL::Schema
   end
 
   # Union and Interface Resolution
-  def self.resolve_type(abstract_type, obj, ctx)
+  def self.resolve_type(_abstract_type, _obj, _ctx)
     # TODO: Implement this method
     # to return the correct GraphQL object type for `obj`
     raise(GraphQL::RequiredImplementationMissingError)
@@ -27,13 +27,13 @@ class WebsiteOneBackendApiSchema < GraphQL::Schema
   # Relay-style Object Identification:
 
   # Return a string UUID for `object`
-  def self.id_from_object(object, type_definition, query_ctx)
+  def self.id_from_object(object, _type_definition, _query_ctx)
     # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
     object.to_gid_param
   end
 
   # Given a string UUID, find the object
-  def self.object_from_id(global_id, query_ctx)
+  def self.object_from_id(global_id, _query_ctx)
     # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
     GlobalID.find(global_id)
   end
